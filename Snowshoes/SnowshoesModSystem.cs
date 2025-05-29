7142e606ba8489198d8c77a57a2ab4ba0f8c75eb
Liviu <Liviu@DESKTOP-LIVIU>
2025-05-29 13:16:13 +0300

    Add project files.

---
+++ Snowshoes/SnowshoesModSystem.cs
@@ -0,0 +1,44 @@
+﻿using Snowshoes.classes;
+using Vintagestory;
+using Vintagestory.API.Client;
+using Vintagestory.API.Common;
+using Vintagestory.API.Config;
+using Vintagestory.API.Server;
+
+namespace Snowshoes
+{
+    public class SnowshoesModSystem : ModSystem
+    {
+        private static ICoreAPI api;
+        public ICoreClientAPI capi;
+        public ICoreServerAPI sapi;
+        private ILogger logger;
+
+        public static SnowshoesModSystem GetInstance() => api.ModLoader.GetModSystem<SnowshoesModSystem>();
+
+        public ILogger Logger {
+            get {  return logger; }
+        }
+
+        // Called on server and client
+        // Useful for registering block/entity classes on both sides
+        public override void Start(ICoreAPI api)
+        {
+            api.RegisterItemClass(Mod.Info.ModID + ".snowshoes", typeof(SnowshoesItem));
+            logger = Mod.Logger;
+            SnowshoesModSystem.api = api;
+        }
+
+        public override void StartServerSide(ICoreServerAPI api)
+        {
+            Mod.Logger.Notification("Hello from template mod server side: " + Lang.Get("snowshoes:hello"));
+            sapi = api;
+        }
+
+        public override void StartClientSide(ICoreClientAPI api)
+        {
+            Mod.Logger.Notification("Hello from template mod client side: " + Lang.Get("snowshoes:hello"));
+            capi = api;
+        }
+    }
+}
---
+++ Snowshoes/classes/SnowshoesItem.cs
@@ -0,0 +1,89 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vintagestory;
+using Vintagestory.API.Client;
+using Vintagestory.API.Common;
+using Vintagestory.API.Common.Entities;
+using Vintagestory.API.Datastructures;
+using Vintagestory.API.MathTools;
+using Vintagestory.API.Util;
+using Vintagestory.Client.NoObf;
+using Vintagestory.Common;
+using Vintagestory.GameContent;
+
+namespace Snowshoes.classes
+{
+    internal class SnowshoesItem : ItemWearable
+    {
+        private static void AlterAnimation(EntityPlayer en, string animationName)
+        {
+            if (animationName == "idle")
+            {
+                if (en.AnimManager.GetAnimationState(animationName + "1") == null
+                    && en.AnimManager.GetAnimationState(animationName + "1-fp") == null) return;
+            }
+
+            if (en.AnimManager.GetAnimationState(animationName) == null
+                && en.AnimManager.GetAnimationState(animationName + "-fp") == null) return;
+
+            Animation customAnim = en.AnimManager.GetAnimationState(animationName).Animation;
+
+            List<string> keys = en.AnimManager.ActiveAnimationsByAnimCode.Keys.ToList();
+            en.World.Logger.Notification(String.Join(", ", keys.ToArray(), 0, keys.Count - 1) + ", and " + keys.LastOrDefault());
+            
+            int i = 0;
+            foreach (AnimationKeyFrame frame in customAnim.KeyFrames)
+            {
+                // Bring model slightly up
+                if (frame.Elements.ContainsKey("LowerTorso"))
+                {
+                    frame.Elements.Get("LowerTorso").OriginY += 1.0;
+                    frame.Elements.Get("LowerTorso").OffsetY += 1.0;
+                }
+
+                // TODO add these elements for idle animation!
+
+                // Spread legs to accomodate shoes
+                if (frame.Elements.ContainsKey("UpperFootL") && frame.Elements.Get("UpperFootL").RotationX != null) 
+                    frame.Elements.Get("UpperFootL").RotationX = -8.0;
+                if (frame.Elements.ContainsKey("LowerFootL") && frame.Elements.Get("LowerFootL").RotationX != null) 
+                    frame.Elements.Get("LowerFootL").RotationX = 6.0;
+                if (frame.Elements.ContainsKey("UpperFootR") && frame.Elements.Get("UpperFootR").RotationX != null) 
+                    frame.Elements.Get("UpperFootR").RotationX = 8.0;
+                if (frame.Elements.ContainsKey("LowerFootR") && frame.Elements.Get("LowerFootR").RotationX != null) 
+                    frame.Elements.Get("LowerFootR").RotationX = -6.0;
+
+                i++;
+            }
+        }
+
+        public override void OnModifiedInInventorySlot(IWorldAccessor world, ItemSlot slot, ItemStack extractedStack = null)
+        {
+            // Called when shoes are placed in feet slot
+            if(slot.GetType() == typeof(ItemSlotCharacter) && extractedStack == null)
+            {
+                if (world.GetType() == typeof(ClientMain))
+                {
+                    ClientMain cm = (ClientMain)world;
+                    EntityPlayer en = cm.Player?.Entity;
+
+                    if (en != null)
+                    {
+                        AlterAnimation(en, "walk");
+                        AlterAnimation(en, "idle");
+                        AlterAnimation(en, "sprint");
+                    }
+                }
+            }
+            
+            base.OnModifiedInInventorySlot(world, slot, extractedStack);
+        }
+    }
+}
